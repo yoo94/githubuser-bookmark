c4cc20e70388ea4633580880e3d13b75fc92396f
yoo94 <ahy0119@naver.com>
2024-10-26 16:15:55 +0900

    -feat: layout setting (헤더,  푸터 및 css)

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      init setting
-    </div>
-  );
-}
